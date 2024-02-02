0bb1ba4cf59b9cd967883d8d3d6a56df96a604fb
lucnietox <luciano.nieto@gmail.com>
2024-02-02 18:24:44 +0000

    darwin and incidents

--- reader_darwin.py
+++ reader_darwin.py
@@ -4,7 +4,7 @@ import gzip
 import xmltodict
 from pymongo import MongoClient
 
-file = open("config.cfg","r")
+file = open("config_darwin.cfg","r")
 cred = json.load(file)
 
 user = cred["cfg_user"]
@@ -21,30 +21,24 @@ def connect_mongo():
 
 ct = connect_mongo()
 db = ct['nationalrail']
-collection = db['incidents_test_real']
-
-#doc = {"id": 4,"name":"teste333","qty":9919}
-#collection.insert_one(doc)
-
-
-
+collection = db['darwin_test_real']
 
 class MyListener(stomp.ConnectionListener):
     def on_error(self, frame):
         print('received an error "%s"' % frame.body)
     def on_message(self, frame):
-        #msg = gzip.decompress(frame.body)
+        msg = gzip.decompress(frame.body)
         #print('received a message "%s"' % msg.decode())
-        xml_data = frame.body.replace("uk.co.nationalrail.xml.incident.PtIncidentStructure","PtIncident") #https://wiki.openraildata.com/KnowledgeBase
+        xml_data = msg.decode() #https://wiki.openraildata.com/KnowledgeBase
         xml_dict = xmltodict.parse(xml_data)
-        print("Message ",xml_dict) 
+        #print("Message ",xml_dict) 
         collection.insert_one(xml_dict)
     def on_connected(self, frame):
         print("CONNECTED")
     def on_connecting(self, host_and_port):
         print("connecting..")
 
-c = stomp.Connection12([(messaging_host, stomp_port)])
+c = stomp.Connection12([(messaging_host, stomp_port)],auto_decode=False)
 c.connect(user, password)
 c.set_listener('', MyListener())
 c.subscribe(destination="/topic/"+live_feed_topic, id=1, ack='auto')
