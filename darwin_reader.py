3e8874390cf4032a84bd468df17fe9dc4d55e26a
lucnietox <luciano.nieto@gmail.com>
2024-02-19 18:01:11 +0000

    final project

--- darwin_reader.py
+++ darwin_reader.py
@@ -3,6 +3,7 @@ import json
 import gzip
 import xmltodict
 from pymongo import MongoClient
+from pykafka import KafkaClient
 
 file = open("config_darwin.cfg","r")
 cred = json.load(file)
@@ -14,23 +15,30 @@ status_message_topic=cred["cfg_status_messages_topic"]
 messaging_host=cred["cfg_messaging_host"]
 stomp_port=cred["cfg_stomp_port"]
 openwire_port=cred["cfg_openwire_port"]
+mpass=cred["mongo_password"]
+muser=cred["mongo_user"]
 
 def connect_mongo():
-    client = MongoClient('localhost', 27017,username="root", password="example")
+    client = MongoClient('localhost', 27017,username=muser, password=mpass)
     return client
 
 ct = connect_mongo()
 db = ct['nationalrail']
-collection = db['darwin_test_real']
+collection = db['darwin']
+
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics[b'nationalrail']
+producer = topic.get_sync_producer()
 
 class MyListener(stomp.ConnectionListener):
     def on_error(self, frame):
-        print('received an error "%s"' % frame.body)
+        print('received an error >>> "%s"' % frame.body)
     def on_message(self, frame):
         msg = gzip.decompress(frame.body)
         xml_data = msg.decode()
         xml_dict = xmltodict.parse(xml_data)
         collection.insert_one(xml_dict["Pport"]["uR"])
+        producer.produce(str(xml_data).encode('utf-8'))
     def on_connected(self, frame):
         print("CONNECTED")
     def on_connecting(self, host_and_port):
--- incidents_reader.py
+++ incidents_reader.py
@@ -2,6 +2,7 @@ import stomp
 import json
 import xmltodict
 from pymongo import MongoClient
+from pykafka import KafkaClient
 
 file = open("config.cfg","r")
 cred = json.load(file)
@@ -13,14 +14,21 @@ status_message_topic=cred["cfg_status_messages_topic"]
 messaging_host=cred["cfg_messaging_host"]
 stomp_port=cred["cfg_stomp_port"]
 openwire_port=cred["cfg_openwire_port"]
+mpass=cred["mongo_password"]
+muser=cred["mongo_user"]
 
 def connect_mongo():
-    client = MongoClient('localhost', 27017,username="root", password="example")
+    client = MongoClient('localhost', 27017,username=muser, password=mpass)
     return client
 
 ct = connect_mongo()
 db = ct['nationalrail']
 collection = db['incidents_test_real']
+
+#kafka
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics[b'nationalrail']
+producer = topic.get_sync_producer()
 
 class MyListener(stomp.ConnectionListener):
     def on_error(self, frame):
@@ -29,6 +37,7 @@ class MyListener(stomp.ConnectionListener):
         xml_data = frame.body.replace("uk.co.nationalrail.xml.incident.PtIncidentStructure","PtIncident") #https://wiki.openraildata.com/KnowledgeBase
         xml_dict = xmltodict.parse(xml_data)
         collection.insert_one(xml_dict)
+        producer.produce(xml_dict)
     def on_connected(self, frame):
         print("CONNECTED")
 
